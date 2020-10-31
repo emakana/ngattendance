ebcfd81471b70f563c82378cf030eff3b3b11e99
Ephraim <ephraim.makana@gmail.com>
2020-10-30 23:05:20 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/httpls';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { LoginsuccessComponent } from './registration/loginsuccess/loginsuccess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    LoginsuccessComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthenticateService } from '../authenticate.service';
+import { User } from '../user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  user = new User();
+  msg = '';
+
+  constructor(private _service: AuthenticateService, private _router: Router) {}
+
+  ngOnInit(): void {}
+
+  loginUser() {
+    this._service.loginUserFromRemote(this.user).subscribe(
+      (data) => {
+        console.log('response received');
+        this._router.navigate(['/loginsuccess']);
+      },
+      (error) => {
+        console.log('exception occurred');
+        this.msg = 'bad credentials, please enter valid email and password';
+      }
+    );
+  }
+}
